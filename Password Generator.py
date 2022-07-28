df7c20ed90b90a32d4055e1f334aaccd716e68e4
Daksh-Bapna <110184283+Daksh-Bapna@users.noreply.github.com>
2022-07-28 20:34:13 +0530

    First Edit

---
+++ Password Generator.py
@@ -0,0 +1,24 @@
+# ____PASSWORD GENERATOR_____
+
+import random
+import string
+i=int(input('Enter the no. of characters you want = '))
+a=(string.ascii_letters)
+d=(string.digits)
+p=(string.punctuation)
+all= a+p+d
+my_random=random.sample(all,i)
+print(''.join(my_random))
+
+
+
+
+# _______________________________________________________________________________________________
+# PASSWORD CHECKER 
+
+# a=input('Enter your Password : ')
+# b=()
+
+
+
+
